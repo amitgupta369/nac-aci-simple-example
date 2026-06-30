1d639174ec26fa45927b881fa77bb41a1bfe8b77
Amit Gupta <gupta-amit>
2026-06-30 21:07:40 +0530

    minor fix

--- providers.tf
+++ providers.tf
@@ -1,7 +1,18 @@
 terraform {
   required_providers {
     aci = {
-      source = "CiscoDevNet/aci"
+      source  = "CiscoDevNet/aci"
+      version = ">= 2.20.0"
+    }
+
+    utils = {
+      source  = "netascode/utils"
+      version = "2.0.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.9.0"
     }
   }
 }
